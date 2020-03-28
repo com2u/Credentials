3f8b4cec78cbe0fe4ce04d3e1f45cedd4f7976d2
Hess Patrick <0812@domsac.de>
2020-03-28 10:56:00 +0100

    Template file

---
+++ Credentials.h
@@ -0,0 +1,9 @@
+#define CREDENTIALS_WIFI_SSID "WLAN"
+#define CREDENTIALS_WIFI_PASSWORD  "PASSWD"
+
+
+char* ssid[] =     {  "muenchen.freifunk.net",  "MyNetwork", "free" };
+char* password[] = {  "",                       "MyPasswed",  "" };
+const char* mqtt_server = "192.168.0.1";
+const char* MQTTUser     = "";
+const char* MQTTPassword = "";
